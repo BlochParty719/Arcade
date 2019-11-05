3251266a25f941d831709f3e89e172bc83746daf
Daniel <danieljbloch95@gmail.com>
2019-11-05 12:33:05 -0500

    updating code for heroku

---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+const express = require('express');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+const app = express ();
+const db = mongoose.connection;
+require('dotenv').config()
+
+// Port
+const Port = process.env.PORT
+
+// Database
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify:
+false})
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middleware
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+// Listener
+app.listen(Port, () => {
+  console.log('Listening on port:', Port);
+})
